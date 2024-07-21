44131a799860b6d55be962896c5f8837c64a87d5
Ibrahim Chikani <147825977+IbrahimDev00@users.noreply.github.com>
2024-07-21 23:08:09 +0530

    Made initial boiler plate

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-white text-2xl text-left">Hi im <span className="text-blue-600 text-2xl text-left">Ibrahim</span></h1>
+    </main>
+  );
+}
