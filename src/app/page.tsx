75952a12a114a80f342cb37fa4762372f537736f
Ibrahim Chikani <147825977+IbrahimDev00@users.noreply.github.com>
2024-07-21 23:25:08 +0530

    Added additional boiler plate code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-
-
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-white text-2xl text-left">Hi im <span className="text-blue-600 text-2xl text-left">Ibrahim</span></h1>
+    <main className="bg-white min-w-full h-full">
+      <div className="text-black text-center font-bold text-3xl">
+        <h1>Hi there <span className="material-symbols-outlined">waving_hand</span><br></br>I'm <span className="text-orange-400">Ibrahim</span></h1>
+      </div>
+
     </main>
   );
 }
