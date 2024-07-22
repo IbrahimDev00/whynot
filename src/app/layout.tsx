462729991823e167fc2e92e8b9af6febcb461d72
Ibrahim Chikani <147825977+IbrahimDev00@users.noreply.github.com>
2024-07-22 20:31:12 +0530

    made changes to landing page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,14 @@ export default function RootLayout({
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0"
         />
+        <link 
+          rel="stylesheet" 
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" 
+        />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
       </head>
       <body style={{ fontFamily: "'Open Sans', sans-serif" }}>
         {children}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,48 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main className="bg-white min-w-full h-full">
-      <div className="text-black text-center font-bold text-3xl">
-        <h1>Hi there <span className="material-symbols-outlined">waving_hand</span><br></br>I'm <span className="text-orange-400">Ibrahim</span></h1>
-      </div>
+    <main className="bg-white min-w-full min-h-screen flex">
+      {/* Navigation */}
+      <nav className="w-1/4 bg-gray-200 p-5 space-y-8">
+        <h1 className="text-black font-semibold sm:text-xl md:text-2xl lg:text-2xl">
+          <span className="material-symbols-outlined">home</span>
+          Home
+        </h1>
+        <h1 className="text-black font-semibold sm:text-xl md:text-2xl lg:text-2xl">
+          <span className="material-symbols-outlined">view_timeline</span>
+          Projects
+        </h1>
+        <h1 className="text-black font-semibold sm:text-xl md:text-2xl lg:text-2xl">
+          <span className="material-symbols-outlined">sentiment_satisfied</span>
+          About me
+        </h1>
+      </nav>
+      
+      {/* Vertical Divider */}
+      <div className="w-px bg-gray-200 h-full"></div>
+      
+      {/* Main Content */}
+      <div className="flex-1 flex flex-col items-center justify-center p-5">
+        <div className="relative flex flex-col items-start">
+          {/* Heading with Gradient Box */}
+          <h1 className="text-black font-semibold text-4xl sm:text-5xl md:text-6xl relative">
+            <div className="absolute top-0 left-0 w-12 h-12 rounded-full bg-gradient-to-tl from-[#12c2e9] via-[#c471ed] to-[#f64f59] animate-flow"></div>
+            <span className="text-gray-500 font-light mr-4 pl-16">01</span> {/* Added padding-left to adjust spacing */}
+            <span className="material-symbols-outlined text-4xl sm:text-5xl md:text-6xl mr-4">arrow_back_ios</span>
+            Hello, I’m&nbsp;<span className="text-indigo-900">Ibrahim!</span>
+            <span className="material-symbols-outlined text-4xl sm:text-5xl md:text-6xl ml-4">arrow_forward_ios</span>
+          </h1>
 
+          {/* Second Heading */}
+          <h1 className="text-black font-semibold text-4xl sm:text-5xl md:text-6xl mt-8">
+            <span className="text-gray-500 font-light mr-4">02</span>
+            <span className="material-symbols-outlined text-4xl sm:text-5xl md:text-6xl mr-4">arrow_back_ios</span>
+            I’m a&nbsp;<span className="text-indigo-900">Student👨‍🎓 & Developer👨‍💻</span>
+            <span className="material-symbols-outlined text-4xl sm:text-5xl md:text-6xl ml-4">arrow_forward_ios</span>
+          </h1>
+        </div>
+      </div>
     </main>
   );
 }
